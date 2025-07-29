7e8be2ec144afd80370d0daf9420c613d6c5554c
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-07-29 14:57:53 +0300

    commit

---
+++ src/task.ems.dal/Entities/Logs/LogHistory.cs
@@ -0,0 +1,8 @@
+﻿namespace task.ems.dal.Entities.Logs;
+
+public sealed record LogHistory : Entity
+{
+    public EntityType EntityType { get; set; }
+    public TransactionType TransactionType { get; set; }
+    public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+}
