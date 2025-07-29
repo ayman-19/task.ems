fc2ade0d8a690c33aef67bca9bff93e727355c96
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-07-29 15:01:07 +0300

    commit

---
+++ src/task.ems.bll/Implementations/Services/Logs/LogHistoryService.cs
@@ -0,0 +1,28 @@
+﻿namespace task.ems.bll.Implementations.Services.Logs;
+
+internal sealed class LogHistoryService(ILogHistoryRepository logHistoryRepository)
+    : ILogHistoryService
+{
+    public async Task<PaginationResponse<IEnumerable<LogHistory>>> PaginateAsync(
+        PaginateLogsRequest request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        int totalCount = await logHistoryRepository.CountAsync(cancellationToken);
+
+        var employees = await logHistoryRepository.PaginateAsync(
+            request.PageIndex,
+            request.PageSize,
+            request.SortDirection,
+            cancellationToken
+        );
+        return new()
+        {
+            Count = employees.Count,
+            PageIndex = request.PageIndex,
+            PageSize = request.PageSize,
+            TotalCount = totalCount,
+            Result = employees.Select(log => log),
+        };
+    }
+}
